55df612f6c3af7a78ace271e82e05cbbb7708ffd
Yedidya Averjel <yedidya.abergel@gmail.com>
2023-05-10 16:07:28 +0300

    Improved validation

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -6,7 +6,7 @@ router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(8).max(30),
     }),
   }),
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -6,7 +6,10 @@ router.post(
   '/',
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().min(2),
+      name: Joi.string().required().min(2).max(30),
+      about: Joi.string().required().min(2).max(30),
+      avatar: Joi.string().required().regex(/^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/),
+      email: Joi.string().required().email(),
       password: Joi.string().required().min(8).max(30),
     }),
   }),
